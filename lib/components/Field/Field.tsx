7e389fd6706d77ec72234c1f80ade446e90debdc
Mark Toh <marktoh96@gmail.com>
2024-02-02 19:14:00 +0800

    feat: switch from flex to grid for Field.tsx

--- lib/components/Field/Field.tsx
+++ lib/components/Field/Field.tsx
@@ -4,8 +4,8 @@ interface FieldProps {
 }
 function Field({ label, value }: FieldProps) {
   return (
-    <div className="flex items-center gap-2">
-      <div className="text-color-primary w-24 self-start text-xs font-medium">
+    <div className="grid grid-cols-[5rem_1fr] items-center gap-2">
+      <div className="text-color-primary self-start text-xs font-medium">
         {label}
       </div>
       <div className="text-color-secondary w-full text-xs font-normal">
