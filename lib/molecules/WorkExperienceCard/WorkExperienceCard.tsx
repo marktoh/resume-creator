75db2d6daa5bf4751b5e27a7f886f8c053fe0207
Mark Toh <marktoh96@gmail.com>
2024-03-14 09:12:23 +0800

    feat: add location and update pdf document

--- lib/molecules/WorkExperienceCard/WorkExperienceCard.tsx
+++ lib/molecules/WorkExperienceCard/WorkExperienceCard.tsx
@@ -1,4 +1,8 @@
 import Plus from "../../../assets/icons/Plus";
+import Briefcase from "../../../assets/icons/Briefcase";
+import BuildingOffice from "../../../assets/icons/BuildingOffice";
+import Calendar from "../../../assets/icons/calendar";
+
 interface WorkExperienceCardProps {
   title: string;
   startDate?: string | null;
@@ -6,6 +10,7 @@ interface WorkExperienceCardProps {
   role?: string;
   description: string | null;
   statements: Array<String>;
+  location?: string;
 }
 function WorkExperienceCard({
   title,
@@ -14,19 +19,27 @@ function WorkExperienceCard({
   role,
   description,
   statements,
+  location,
 }: WorkExperienceCardProps) {
   return (
     <div className="flex flex-col">
       <div className="flex items-center">
         <h3 className="text-color-primary text-base font-semibold">{title}</h3>
         {role && (
-          <span className="text-color-secondary ml-0.5 mt-0.5 rounded px-1 py-1 text-[13px] font-medium leading-4 tracking-wide">
-            {role}
+          <span className="text-color-secondary ml-1.5 mt-0.5 flex items-center gap-0.5 rounded px-1 py-1 text-[12px] font-medium leading-4 tracking-wide">
+            <Briefcase /> {role}
           </span>
         )}
-        <span className="text-color-secondary ml-0.5 mt-0.5 text-[12px] font-medium tracking-wider">
-          {startDate && endDate && `${startDate} - ${endDate}`}
-        </span>
+        {startDate && endDate && (
+          <span className="text-color-secondary ml-1.5 mt-0.5 flex items-center gap-0.5 text-[12px] font-medium tracking-wide">
+            <Calendar /> {`${startDate} - ${endDate}`}
+          </span>
+        )}
+        {location && (
+          <span className="text-color-secondary ml-1.5 mt-0.5 flex items-center gap-0.5 text-[12px] font-medium tracking-wide">
+            <BuildingOffice /> {location}
+          </span>
+        )}
       </div>
       <div className="text-color-secondary">
         <p className="text-color-tertiary text-xs">{description}</p>
